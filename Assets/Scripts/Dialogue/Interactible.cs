181d40a5e62791f7db367e28531247ef90131a6a
Microraptor13 <mpm3256@gmail.com>
2023-09-28 13:08:26 -0700

    Made Dialogue NPCs
    Updated Dialogue Scene
    Updated dialogue system to work
    Implemented NPC prefabs for easy NPC creation

--- Assets/Scripts/Dialogue/Interactible.cs
+++ Assets/Scripts/Dialogue/Interactible.cs
@@ -13,4 +13,47 @@ using UnityEngine;
 public class Interactible : MonoBehaviour
 {
     public DialogueData interactionData;
+    public GameObject talkDisplay;
+    [SerializeField]
+    private bool seePlayer;
+    private PlayerInputs playerInputs;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            seePlayer = true;
+            playerInputs = collision.GetComponent<PlayerInputs>();
+            talkDisplay.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            seePlayer = false;
+            playerInputs = null;
+            talkDisplay.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (seePlayer)
+        {
+            
+            if (playerInputs.OnPress(PlayerInputs.PlayerAction.Interact))
+            {
+                if (DialogueWindow.Current.dialoguePanel.activeSelf)
+                {
+                    DialogueWindow.Current.AdvanceText();
+                }
+                else
+                {
+                    DialogueWindow.Current.DisplayDialogue(interactionData);
+                }
+            }
+        }
+    }
 }
