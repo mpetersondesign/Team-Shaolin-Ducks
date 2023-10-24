93a54b78136cd09d8fbcf1e8ba229b2868773c69
Microraptor13 <mpm3256@gmail.com>
2023-10-24 13:05:17 -0700

    Added Text Scroll

--- Assets/Scripts/Dialogue/DialogueWindow.cs
+++ Assets/Scripts/Dialogue/DialogueWindow.cs
@@ -21,9 +21,17 @@ public class DialogueWindow : MonoBehaviour
     public Image characterPortrait;
     public TextMeshProUGUI characterNameDisplay;
     public TextMeshProUGUI dialogueDisplay;
+    public int textDelay = 1;
+    private int textDelayTimer = 0;
 
+    [SerializeField]
     private DialogueData currentDialogue;
     private int currentLine = 0;
+
+    private int numChar;
+    private int currentChar = 0;
+    private string currentDisplayText;
+    private bool scrollingText = false;
 
     public void Awake()
     {
@@ -53,9 +61,12 @@ public class DialogueWindow : MonoBehaviour
 
     public void UpdateText()
     {
+        numChar = currentDialogue.Lines[currentLine].Text.Length;
+        SetUpDisplayText();
         characterPortrait.sprite = currentDialogue.Speaker.portrait;
         characterNameDisplay.text = currentDialogue.Speaker.characterName;
-        dialogueDisplay.text = currentDialogue.Lines[currentLine].Text;
+        dialogueDisplay.text = currentDisplayText;
+        scrollingText = true;
     }
 
     public void AdvanceText()
@@ -74,6 +85,50 @@ public class DialogueWindow : MonoBehaviour
                 FindObjectOfType<PlayerController>().CanMove = true;
                 // Let Player Move Again
             }
+        }
+    }
+
+    public void SetUpDisplayText()
+    {
+        currentChar = 0;
+        currentDisplayText = "";
+        for (int i = 0; i < numChar; i++)
+        {
+            currentDisplayText += " ";
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (scrollingText)
+        {
+            if(textDelayTimer % textDelay == 0)
+            {
+                TextScrollUpdate();
+                dialogueDisplay.text = currentDisplayText;
+            }
+            if(textDelayTimer > textDelay)
+            {
+                textDelayTimer = 0;
+            }
+            else
+            {
+                textDelayTimer++;
+            }
+        }
+        
+    }
+
+    public void TextScrollUpdate()
+    {
+        if (currentChar < numChar)
+        {
+            currentDisplayText = currentDialogue.Lines[currentLine].Text.Substring(0, currentChar);
+            currentChar++;
+        }
+        else
+        {
+            scrollingText = false;
         }
     }
 }
