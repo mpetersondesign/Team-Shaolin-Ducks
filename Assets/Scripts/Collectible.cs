e2c308f7cf7546c501df766a98df6f0bee640c7a
Microraptor13 <mpm3256@gmail.com>
2023-10-26 12:59:24 -0700

    Implemented Collectibles

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Collectible : MonoBehaviour
+{
+
+    [SerializeField]
+    private UnityEvent onCollect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            onCollect.Invoke();
+        }
+    }
+}
