657705bea467b1ff3e25ea5e50e142ca003cf795
Sevithian <m.peterson@digipen.edu>
2023-11-28 17:15:30 -0800

    Tweaked some level specific variables

--- Assets/LevelTrigger.cs
+++ Assets/LevelTrigger.cs
@@ -12,6 +12,7 @@ public class LevelTrigger : MonoBehaviour
     public LevelTrigger EndTrigger;
     public bool LevelStarted = false;
     public Vector3 PlayerReturnPos;
+    public string LevelName;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,7 +21,7 @@ public class LevelTrigger : MonoBehaviour
             LevelStarted = true;
             EndTrigger.LevelStarted = true;
             FindObjectOfType<GameSystems>().StartTimer(Level);
-            FindObjectOfType<GameSystems>().AreaChange("Jason's Level");
+            FindObjectOfType<GameSystems>().AreaChange(LevelName);
         }
 
         if (collision.tag == "Player" && LevelStarted && Type == TriggerType.End)
